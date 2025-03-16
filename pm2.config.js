7e92be99ce9e1b73bc35384c3900e2e5502814fb
Abdul Rojak <bigzero55@outlook.com>
2025-03-16 08:59:56 +0700

    feat: update start script to specify port and clean up pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -1,21 +1,18 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 const pkg = require("./package.json");
 require("dotenv").config();
 
 module.exports = {
   apps: [
     {
-      name: pkg.name, // Menggunakan nama dari package.json
+      name: pkg.name,
       script: "npm",
       args: "start",
       exec_mode: "cluster",
       instances: -1,
-      env: {
-        PORT: process.env.PORT,
-        NODE_ENV: process.env.NODE_ENV,
-      },
     },
     {
-      name: `${pkg.name}_storage`, // Menambahkan suffix '_storage' ke nama aplikasi
+      name: `${pkg.name}_storage`,
       script: "./storage_server.js",
       exec_mode: "cluster",
       instances: 1,
