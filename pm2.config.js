5efd04623ddf4fb16ffc51fcf0578ae211038432
Abdul Rojak <bigzero55@outlook.com>
2025-03-24 17:51:13 +0700

    feat: update PM2 configuration to use fork mode and set environment ports

--- pm2.config.js
+++ pm2.config.js
@@ -5,19 +5,22 @@ require("dotenv").config();
 module.exports = {
   apps: [
     {
-      name: pkg.name,
+      name: pkg.name, // Misalnya "voice"
       script: "npm",
       args: "start",
-      exec_mode: "cluster",
-      instances: -1,
+      exec_mode: "fork", // Ganti ke "fork" agar tidak ada konflik port
+      instances: 1, // Hanya satu instance untuk Next.js
+      env: {
+        PORT: process.env.PORT || "4000", // Gunakan PORT dari .env atau default ke 4000
+      },
     },
     {
-      name: `${pkg.name}_storage`,
+      name: `${pkg.name}_storage`, // Misalnya "voice_storage"
       script: "./storage_server.js",
-      exec_mode: "cluster",
-      instances: 1,
+      exec_mode: "fork", // Gunakan "fork" untuk simplicitiy, kecuali clustering diperlukan
+      instances: 1, // Tetap satu instance untuk storage
       env: {
-        PORT: process.env.STORAGE_PORT,
+        PORT: process.env.STORAGE_PORT || "4001", // Port berbeda dari Next.js
       },
     },
   ],
